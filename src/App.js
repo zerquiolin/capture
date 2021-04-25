70ecae12ecd84f2e79517fbdbf38a111a5e70539
zerquiolin <handres47@hotmail.com>
2021-04-25 16:42:34 -0500

    URL fix

--- src/App.js
+++ src/App.js
@@ -21,16 +21,16 @@ function App() {
       <Nav />
       <AnimatePresence exitBeforeEnter>
         <Switch location={location} key={location.pathname}>
-          <Route path="/" exact>
+          <Route path="/capture/" exact>
             <AboutUs />
           </Route>
-          <Route path="/work" exact>
+          <Route path="/capture/work" exact>
             <OurWork />
           </Route>
-          <Route path="/work/:id">
+          <Route path="/capture/work/:id">
             <MovieDetails />
           </Route>
-          <Route path="/contact">
+          <Route path="/capture/contact">
             <ContactUs />
           </Route>
         </Switch>
